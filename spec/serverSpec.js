984f6ba6d9918dca49679c9d9d386ae60f4ce137
lmarquardt7 <lmarquardt7@student.cvtc.edu>
2023-03-07 23:43:17 -0600

    added to tests

--- spec/serverSpec.js
+++ spec/serverSpec.js
@@ -22,15 +22,39 @@ describe('server', function () {
   describe("GET '/'", function () {
     shouldBeAbove200('/')
   })
+  describe("GET '/about'", function () {
+    shouldBeAbove200('/about')
+  })
+  describe("GET '/list'", function () {
+    shouldBeAbove200('/list')
+  })
+  describe("GET '/search'", function () {
+    shouldBeAbove200('/search')
+  })
+  describe("GET '/login'", function () {
+    shouldBeAbove200('/login')
+  })
   describe("GET '/health'", function () {
     shouldBeLessThan399('/health')
   })
   describe("GET '/'", function () {
     shouldBeLessThan399('/')
   })
+  describe("GET '/about'", function () {
+    shouldBeLessThan399('/about')
+  })
+  describe("GET '/list'", function () {
+    shouldBeLessThan399('/list')
+  })
+  describe("GET '/search'", function () {
+    shouldBeLessThan399('/search')
+  })
+  describe("GET '/login'", function () {
+    shouldBeLessThan399('/login')
+  })
   describe("POST '/user'", function () {
     const url = new URL('/user', baseUrl)
-    it('should accept user information', async function () {
+    it('should accept valid user information', async function () {
       const data = {
         username: 'userName1',
         password: 'Password1',
@@ -50,6 +74,14 @@ describe('server', function () {
 
       const results = await response.json()
       expect(results.ok).toBeTrue()
+    })
+  })
+  describe('queryAllMovies', function () {
+    it('should return results', async function () {
+      const results = await queryAllMovies()
+      expect(results).toBeDefined()
+      expect(results.movies).toBeDefined()
+      expect(results.movies.length).toBeGreaterThan(0)
     })
   })
 })
